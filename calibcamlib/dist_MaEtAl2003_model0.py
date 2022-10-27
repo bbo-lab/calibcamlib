a21de484164b9725e38f6991b7868385d46e2f42
Kay-Michael Voit <kay-michael.voit@caesar.de>
2022-10-27 13:42:21 +0200

    Reused code from Opencv for other distortion module, mostly for simplification and documentation purposes

--- calibcamlib/dist_MaEtAl2003_model0.py
+++ calibcamlib/dist_MaEtAl2003_model0.py
@@ -1,24 +1,13 @@
 # implements model0 from MaEtAl2003
-
 import numpy as np
+import calibcamlib.dist_OpenCV
 
 
 def distort(abi, k):
-    s2 = np.sum(abi ** 2, axis=1)
-    return (1 + k[0] * s2 + k[1] * s2 ** 2)[:, np.newaxis] * abi
+    assert np.all(k[2:] == 0), "Only the first 2 k entries may differ from 0"
+    return calibcamlib.dist_OpenCV.distort(abi, k)
 
 
 def distort_inverse(ab_rd, k):
-    n = ab_rd.shape[0]
-    s = np.sqrt(np.sum(ab_rd ** 2, axis=1))
-    r = np.zeros(n)
-
-    for u in np.where(s > 0)[0]:
-        rts = np.roots(np.array([k[1], 0, k[0], 0, 1, -s[u]]))
-        rtsind = np.all([np.imag(rts) == 0, rts >= 0], axis=0)
-        if not np.any(rtsind):
-            r[u] = np.nan
-        else:
-            r[u] = np.min(np.real(rts[rtsind]))
-
-    return ab_rd * (r / s)[:, np.newaxis]
+    assert np.all(k[2:] == 0), "Only the first 2 k entries may differ from 0"
+    return calibcamlib.dist_OpenCV.distort_inverse(abi, k)
