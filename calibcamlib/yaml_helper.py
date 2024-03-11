de59b28b245df6dc3a65b1e9294df951538d6c99
Kay-Michael Voit <kay-michael.voit@mpinb.mpg.de>
2024-03-11 15:00:50 +0100

    Fixed bug where preparation for yaml export alters calibration

--- calibcamlib/yaml_helper.py
+++ calibcamlib/yaml_helper.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 from copy import deepcopy
 
@@ -23,18 +24,19 @@ def numpy_collection_to_list(collection):
     return deepcopy(collection)
 
 
-def collection_to_array(iput):
-    if isinstance(iput, list):
-        if np.array(iput).dtype == "O":
+def collection_to_array(input):
+    input = copy.deepcopy(input)
+    if isinstance(input, list):
+        if np.array(input).dtype == "O":
             new_list = []
-            for i in iput:
+            for i in input:
                 new_list.append(collection_to_array(i))
             return new_list
         else:
-            return np.array(iput)
-    elif isinstance(iput, dict):
-        for ky in iput.keys():
-            iput[ky] = collection_to_array(iput[ky])
-        return iput
+            return np.array(input)
+    elif isinstance(input, dict):
+        for ky in input.keys():
+            input[ky] = collection_to_array(input[ky])
+        return input
     else:
-        return iput
+        return input
