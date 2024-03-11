68364e1859c395a24faf4ca0f22b61922e81ba05
Kay-Michael Voit <kay-michael.voit@mpinb.mpg.de>
2024-03-11 15:08:00 +0100

    Fixed bug where preparation for yaml export alters calibration - Attempt 2

--- calibcamlib/yaml_helper.py
+++ calibcamlib/yaml_helper.py
@@ -1,42 +1,43 @@
-import copy
 import numpy as np
 from copy import deepcopy
 
 
-def numpy_collection_to_list(collection):
+def numpy_collection_to_list(collection, do_copy=True):
+    if do_copy:
+        collection = deepcopy(collection)
     if isinstance(collection, np.ndarray):
         return collection.tolist()
     elif isinstance(collection, dict):
         for k in collection.keys():
-            collection[k] = numpy_collection_to_list(collection[k])
+            collection[k] = numpy_collection_to_list(collection[k], do_copy=False)
         return collection
     elif isinstance(collection, list):
-        return [numpy_collection_to_list(le) for le in collection]
+        return [numpy_collection_to_list(le, do_copy=False) for le in collection]
     elif isinstance(collection, tuple):
-        return tuple(numpy_collection_to_list(le) for le in collection)
+        return tuple(numpy_collection_to_list(le, do_copy=False) for le in collection)
     elif hasattr(collection, 'dtype'):
         # To convert numpy float64 and int64 to float and int
         if np.issubdtype(collection.dtype, np.integer):
             return int(collection)
         elif np.issubdtype(collection.dtype, np.floating):
             return float(collection)
+    return collection
 
-    return deepcopy(collection)
 
-
-def collection_to_array(input):
-    input = copy.deepcopy(input)
+def collection_to_array(input, do_copy=True):
+    if do_copy:
+        input = deepcopy(input)
     if isinstance(input, list):
         if np.array(input).dtype == "O":
             new_list = []
             for i in input:
-                new_list.append(collection_to_array(i))
+                new_list.append(collection_to_array(i), do_copy=False)
             return new_list
         else:
             return np.array(input)
     elif isinstance(input, dict):
         for ky in input.keys():
-            input[ky] = collection_to_array(input[ky])
+            input[ky] = collection_to_array(input[ky], do_copy=False)
         return input
     else:
         return input
