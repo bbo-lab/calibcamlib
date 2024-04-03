3dad67566d67e94b3b6d52285db380db55d598be
Kay-Michael Voit <kay-michael.voit@mpinb.mpg.de>
2024-04-03 15:26:28 +0200

    Changes to remove python specific entries in YAML

--- calibcamlib/yaml_helper.py
+++ calibcamlib/yaml_helper.py
@@ -14,7 +14,10 @@ def numpy_collection_to_list(collection, do_copy=True):
     elif isinstance(collection, list):
         return [numpy_collection_to_list(le, do_copy=False) for le in collection]
     elif isinstance(collection, tuple):
-        return tuple(numpy_collection_to_list(le, do_copy=False) for le in collection)
+        return [numpy_collection_to_list(le, do_copy=False) for le in collection]
+    elif isinstance(collection, bytes):
+        input = collection.decode().strip()
+        return input
     elif hasattr(collection, 'dtype'):
         # To convert numpy float64 and int64 to float and int
         if np.issubdtype(collection.dtype, np.integer):
@@ -36,6 +39,10 @@ def collection_to_array(input, do_copy=True):
         else:
             return np.array(input)
     elif isinstance(input, dict):
+        for ky in input.keys():
+            input[ky] = collection_to_array(input[ky], do_copy=False)
+        return input
+    elif isinstance(input, tuple):
         for ky in input.keys():
             input[ky] = collection_to_array(input[ky], do_copy=False)
         return input
